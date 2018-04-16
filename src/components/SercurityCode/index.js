ab5692a4f39a4b633a4f1997105588102f2778cb
Aaron Chen <chen_dong85@foxmail.com>
2018-04-16 11:45:35 +0800

    spike the solution about the silder view

---
+++ src/components/SercurityCode/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { SlidingPane, SlidingPaneWrapper } from 'react-native-sliding-panes';
+
+class SecretCode extends Component {
+  componentDidMount() {
+    this.pane1.warpCenter();
+    this.pane2.warpRight();
+    this.pane3.warpRight();
+    this.slidingPaneWrapper.childPanes = [this.pane1, this.pane2, this.pane3];
+  }
+
+  render() {
+    return (
+      <SlidingPaneWrapper
+        style={{ flex: 1 }}
+        ref={slidingPaneWrapper => {
+          this.slidingPaneWrapper = slidingPaneWrapper;
+        }}
+      >
+        <SlidingPane
+          style={[{ borderColor: '#FF9999', borderWidth: 2 }]}
+          ref={pane1 => {
+            this.pane1 = pane1;
+          }}
+        >
+          <View>
+            <Text>Pane 1</Text>
+            <Button
+              onPress={() => this.slidingPaneWrapper.setActive(2)}
+              title={'This is the Button'}
+            />
+          </View>
+        </SlidingPane>
+        <SlidingPane
+          style={[{ borderColor: '#FF9999', borderWidth: 2 }]}
+          ref={pane2 => {
+            this.pane2 = pane2;
+          }}
+        >
+          <View>
+            <Text>Pane 2</Text>
+          </View>
+        </SlidingPane>
+        <SlidingPane
+          style={[{ borderColor: '#FF9999', borderWidth: 2 }]}
+          ref={pane3 => {
+            this.pane3 = pane3;
+          }}
+        >
+          <View>
+            <Text>Pane 3</Text>
+          </View>
+        </SlidingPane>
+      </SlidingPaneWrapper>
+    );
+  }
+}
+
+export default SecretCode;
---
+++ src/components/SercurityCode/index.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import SecretCode from './index';
+
+storiesOf('Sercurity code', module).add('default', () => <SecretCode />);
