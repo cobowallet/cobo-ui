43ab627ee11305cb4eeabcec03713736b4b163f7
Aaron Chen <chen_dong85@foxmail.com>
2018-04-19 10:18:37 +0800

    move the font into the them and add the default value

--- src/components/Core/CBText/index.js
+++ src/components/Core/CBText/index.js
@@ -1,19 +1,6 @@
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
-
-const FontColors = {
-  gray: '#343a40',
-  grayLight: '#8F95AA',
-  red: '#f03e3e',
-  violet: '#7950f2',
-  white: '#fff',
-  primary: '#3A5ADB',
-  dark: '#212529',
-  blue: '#5170EB',
-  disabledGray: '#8F95AA',
-  orange: '#FFC22F',
-  green: '#00B191',
-};
+import { FontColors } from '../../../theme/CBColor';
 
 const CBText = styled.Text`
   font-size: ${props => (props.small ? 13 : 15)};
@@ -36,6 +23,7 @@ CBText.propTypes = {
 
 CBText.defaultProps = {
   color: 'gray',
+  colorHex: '#000',
 };
 
 export default CBText;
