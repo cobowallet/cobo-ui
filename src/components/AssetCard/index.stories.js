d259bfc36f59a139d7029f184d5ede2af29dfe46
wufd <dong.wu@cobo.com>
2018-04-23 20:40:46 +0800

    add asset card component

---
+++ src/components/AssetCard/index.stories.js
@@ -0,0 +1,74 @@
+import React, { PureComponent } from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { View, Text } from 'react-native';
+import { action } from '@storybook/addon-actions';
+import AssetCard from './AssetCard';
+
+class AssetCardTest extends PureComponent {
+  render() {
+    const wallets = [
+      {
+        coinCode: 'ETH',
+        amount: '4.800',
+        fiatCurrencyAmount: '5220.96',
+        fiatCurrencySymbol: '$',
+        txList: [{ title: 'Received' }, { title: 'Sent' }],
+      },
+      {
+        coinCode: 'ETH',
+        amount: '4.800',
+        fiatCurrencyAmount: '5220.96',
+        fiatCurrencySymbol: '$',
+        txList: [{ title: 'Received' }, { title: 'Sent' }],
+      },
+      {
+        coinCode: 'ETH',
+        amount: '4.800',
+        fiatCurrencyAmount: '5220.96',
+        fiatCurrencySymbol: '$',
+        txList: [{ title: 'Received' }, { title: 'Sent' }],
+      },
+    ];
+    return (
+      <View
+        style={{ flex: 1, paddingHorizontal: 16, paddingVertical: 16, backgroundColor: '#f5f5f6' }}
+      >
+        {wallets.map((item, index) => {
+          return (
+            <AssetCard {...item} onPress={action('assetCard did click')} key={index.toString()}>
+              {item.txList &&
+                item.txList.length > 0 &&
+                item.txList.map((txItem, txIndex) => {
+                  return (
+                    <View
+                      key={txIndex.toString()}
+                      style={{
+                        height: 50,
+                        width: '100%',
+                        borderTopColor: '#E3EAF1',
+                        borderTopWidth: 1,
+                        justifyContent: 'center',
+                        paddingHorizontal: 16,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: '400',
+                          fontSize: 15,
+                          color: 'black',
+                        }}
+                      >
+                        {txItem.title}
+                      </Text>
+                    </View>
+                  );
+                })}
+            </AssetCard>
+          );
+        })}
+      </View>
+    );
+  }
+}
+
+storiesOf('AssetCard', module).add('default', () => <AssetCardTest />);
---
+++ src/components/AssetCard/style.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { CBShadow, CBText } from '../Core';
+import CBColors from '../../theme/CBColor';
+import { WalletLogo } from '../../icons';
+
+export const Container = styled(CBShadow)`
+  background-color: white;
+  border-radius: 3;
+  margin-vertical: 8;
+  elevation: 1;
+  shadow-color: ${CBColors.grayLight};
+`;
+
+export const Row = styled.TouchableOpacity`
+  width: 100%;
+  height: 72;
+  padding-left: 16;
+  flex-direction: row;
+  align-items: center;
+  background-color: transparent;
+`;
+
+export const IconContent = styled.View`
+  justify-content: center;
+`;
+
+export const CoinIcon = styled(WalletLogo)`
+  height: 46;
+  width: 46;
+`;
+
+export const AmountContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const AmountText = styled(CBText)`
+  font-weight: 800;
+  font-size: 17;
+  margin-bottom: 5;
+`;
+
+export const ToggleArea = styled.TouchableOpacity`
+  width: 50;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
