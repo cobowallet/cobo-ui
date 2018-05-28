dd48d61e37b3cf704d62691b323757ed1276bbe4
wufd <dong.wu@cobo.com>
2018-05-28 11:21:07 +0800

    update CoinLogo ; update TxRow style

--- src/components/TransactionRecordRow/index.js
+++ src/components/TransactionRecordRow/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { Image, View } from 'react-native';
+import { View } from 'react-native';
 import { CBText } from '../Core';
-import { SendIcon, ReceiveIcon } from '../../icons';
+import { SendIcon, ReceiveIcon, RewardIcon } from '../../icons';
 import TransactionStatus from '../TransactionStatus';
 
 const Container = styled.TouchableOpacity`
@@ -13,17 +13,20 @@ const Container = styled.TouchableOpacity`
   align-items: center;
   padding-left: 16;
   padding-right: 16;
-  padding-top: 16;
-  padding-bottom: 16;
+  padding-top: 14;
+  padding-bottom: 14;
 `;
 
-const Icon = ({ isSendOut }) => {
+const Icon = ({ isSendOut, action }) => {
   const styled = {
-    width: 50,
-    height: 50,
-    marginEnd: 12,
+    width: 42,
+    height: 42,
+    marginRight: 12,
   };
 
+  if (action === 'recv_pos_dividend') {
+    return <RewardIcon style={styled} />;
+  }
   if (isSendOut) {
     return <SendIcon type={'transaction'} style={styled} />;
   } else {
@@ -46,18 +49,19 @@ const TransactionRecordRow = ({
   amount,
   message,
   coinCode,
+  action,
   extra,
   style,
   onPress,
 }) => (
   <Container onPress={onPress} style={style}>
-    {showIcon && <Icon isSendOut={isSendOut} />}
+    {showIcon && <Icon isSendOut={isSendOut} action={action} />}
 
     <View style={{ flex: 1 }}>
       <CBText bold color="dark">
         {title}
       </CBText>
-      <CBText small color="grayLight" style={{ marginTop: 4 }}>
+      <CBText small color="grayLight" style={{ marginTop: 7 }}>
         {message}
       </CBText>
     </View>
@@ -67,9 +71,9 @@ const TransactionRecordRow = ({
         isSendOut ? '-' : '+'
       }${amount} ${coinCode}`}</CBText>
       {typeof extra === 'string' ? (
-        <TimeText time={extra} style={{ marginTop: 4 }} />
+        <TimeText time={extra} style={{ marginTop: 7 }} />
       ) : (
-        <TransactionStatus {...extra} />
+        <TransactionStatus {...extra} style={{ marginTop: 7 }} />
       )}
     </View>
   </Container>
@@ -77,11 +81,12 @@ const TransactionRecordRow = ({
 
 TransactionRecordRow.propTypes = {
   isSendOut: PropTypes.bool.isRequired,
-  showIcon: PropTypes.bool.isRequired,
+  showIcon: PropTypes.bool,
   title: PropTypes.string.isRequired,
   amount: PropTypes.string.isRequired,
   message: PropTypes.string.isRequired,
   coinCode: PropTypes.string.isRequired,
+  action: PropTypes.string,
   extra: PropTypes.oneOfType([
     PropTypes.shape({
       status: PropTypes.string.isRequired,
@@ -96,6 +101,7 @@ TransactionRecordRow.propTypes = {
 TransactionRecordRow.defaultProps = {
   showIcon: true,
   style: {},
+  action: '',
   onPress: () => {},
 };
 
