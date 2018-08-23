a328ad1d665a07fe67abf40dacf1acd12f807d47
wufd <dong.wu@cobo.com>
2018-08-23 15:57:16 +0800

    fix bug
    Summary: fix StatusColors 找不到的情况下 APPcrash
    
    Test Plan: null
    
    Reviewers: liukai, hwang, chendong, niegx
    
    Reviewed By: chendong
    
    Subscribers: tql, cjiang, qsima
    
    Differential Revision: https://dev.bitpao.com/D1904

--- src/components/TransactionStatus/index.js
+++ src/components/TransactionStatus/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
-import { isNil } from 'ramda';
 import { CBText } from '../Core';
 
 export const StatusColors = {
@@ -13,13 +12,13 @@ export const StatusColors = {
   expired: { font: '#8F95AA', background: '#EBEDF6' },
   partial_complete: { font: '#8F95AA', background: '#EBEDF6' },
   partial_complete_canceled: { font: '#8F95AA', background: '#EBEDF6' },
+  droped: { font: '#8F95AA', background: '#EBEDF6' },
   other: { font: '#8F95AA', background: '#EBEDF6' },
 };
 
 const StatusContent = styled.View`
   border-radius: 3;
-  background-color: ${props =>
-    StatusColors[props.status].background || StatusColors.other.background};
+  background-color: ${props => (StatusColors[props.status] || StatusColors.other).background};
   justify-content: center;
   align-items: center;
   padding-left: 8;
@@ -30,7 +29,7 @@ const StatusContent = styled.View`
 
 const StatusText = CBText.extend`
   font-size: 10;
-  color: ${props => StatusColors[props.status].font || StatusColors.other.font};
+  color: ${props => (StatusColors[props.status] || StatusColors.other).font};
 `;
 
 function TransactionStatus({ style, status, statusText }) {
