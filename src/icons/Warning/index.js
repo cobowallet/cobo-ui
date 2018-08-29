781ad17f161b01656bf4e7bf0f9c2744d685804d
Robin <robin.zhangx@gmail.com>
2018-08-29 15:40:36 +0800

    add gray warning icon.
    Summary: as title
    
    Test Plan: n/a
    
    Reviewers: chendong, liukai, wufd
    
    Reviewed By: wufd
    
    Differential Revision: https://dev.bitpao.com/D1969

--- src/icons/Warning/index.js
+++ src/icons/Warning/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+import blue from './img/warning.png';
+import gray from './img/warning_gray.png';
 
-function Warning({ style }) {
-  return <Image style={style} source={require('./img/warning.png')} />;
+const ICON = {
+  blue,
+  gray,
+};
+
+function Warning({ style, type }) {
+  return <Image style={style} source={ICON[type]} />;
 }
 
 export default Warning;
+
+Warning.propTypes = {
+  style: PropTypes.object,
+  type: PropTypes.oneOf(['blue', 'gray']),
+};
+
+Warning.defaultProps = {
+  style: {},
+  type: 'blue',
+};
