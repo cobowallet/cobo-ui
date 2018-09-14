3d5e3529b3610ede1ac81d25cfc0490789560a93
wufd <dong.wu@cobo.com>
2018-09-14 11:08:00 +0800

    add search icon
    Summary: 添加一个搜索icon
    
    Test Plan: null
    
    Reviewers: hwang, chendong, niegx
    
    Reviewed By: niegx
    
    Subscribers: tql, cjiang, qsima
    
    Differential Revision: https://dev.bitpao.com/D2141

--- src/icons/Search/index.js
+++ src/icons/Search/index.js
@@ -4,11 +4,13 @@ import { Image } from 'react-native';
 import normal from './img/search.png';
 import inline from './img/search-inline.png';
 import bar from './img/search-bar.png';
+import notSearch from './img/not-search.png';
 
 const imgs = {
   normal,
   inline,
   bar,
+  notSearch,
 };
 
 export default function Search({ inline, type, style }) {
@@ -20,7 +22,7 @@ export default function Search({ inline, type, style }) {
 
 Search.propTypes = {
   inline: PropTypes.bool,
-  type: PropTypes.oneOf(['normal', 'inline', 'bar']),
+  type: PropTypes.oneOf(['normal', 'inline', 'bar', 'notSearch']),
 };
 
 Search.defaultProps = {
