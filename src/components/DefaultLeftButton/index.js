60a87c18f02f7a7849f6b95c1a054572a37318d4
wufd <dong.wu@cobo.com>
2018-10-09 15:51:11 +0800

    fix 导航右按钮展示问题
    Summary: 调UI
    
    Test Plan: null
    
    Reviewers: hwang, chendong, niegx
    
    Reviewed By: niegx
    
    Subscribers: tql, cjiang, qsima
    
    Differential Revision: https://dev.bitpao.com/D2260

--- src/components/DefaultLeftButton/index.js
+++ src/components/DefaultLeftButton/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Icon } from 'CoboUI';
+import { Ionicons } from '../../icons';
 
 const Container = styled.TouchableOpacity`
   padding-horizontal: 16;
-  padding-vertical: 16;
+  height: 100%;
+  justify-content: center;
 `;
 
 export default function DefaultLeftButton({ onPress }) {
   return (
     <Container onPress={onPress}>
-      <Icon.Ionicons name="ios-arrow-back" size={25} color="#8F95AA" />
+      <Ionicons name="ios-arrow-back" size={25} color="#8F95AA" />
     </Container>
   );
 }
