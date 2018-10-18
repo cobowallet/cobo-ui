ce75f5b0f2583d6684d5a175e853b2fe9abd0cb9
Aaron Chen <chen_dong85@foxmail.com>
2018-10-18 14:19:23 +0800

    add the new icon file
    Summary:
    add the new Icon for message
    
    change the file name for sign
    
    Test Plan: n/a
    
    Reviewers: wufd, niegx
    
    Reviewed By: wufd
    
    Differential Revision: https://dev.bitpao.com/D2342

--- src/icons/SignMessage/index.js
+++ src/icons/SignMessage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image } from 'react-native';
 
 function SignMessage() {
-  return <Image source={require('./img/sort.png')} />;
+  return <Image source={require('./img/sign.png')} />;
 }
 
 export default SignMessage;
