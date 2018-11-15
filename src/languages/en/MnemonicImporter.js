8d38eec5339e4c6a93d78a265cd7d75d47c7dc1d
Aaron Chen <chen_dong85@foxmail.com>
2018-11-15 20:48:44 +0800

    change the words
    Summary: change the words on hd imported
    
    Test Plan: as in view
    
    Reviewers: qding
    
    Reviewed By: qding
    
    Differential Revision: https://dev.bitpao.com/D2643

--- src/languages/en/MnemonicImporter.js
+++ src/languages/en/MnemonicImporter.js
@@ -1,9 +1,7 @@
 const MnemonicImporter = {
   importPage: {
     header: 'Recover HD wallet',
-    descriptions: [
-      "Please enter the 12 words Recovery Phrase, use 'space' to move on to the next word.",
-    ],
+    descriptions: ["Please enter the Recovery Phrase, use 'space' to move on to the next word."],
     button: 'Start to Recover',
   },
 };
