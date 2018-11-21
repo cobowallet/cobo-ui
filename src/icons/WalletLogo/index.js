123b814c839eae89cc7ef0061cf7b8df73f3fa1a
wufd <dong.wu@cobo.com>
2018-11-21 14:13:49 +0800

    修改wallet icon显示逻辑
    Summary:
    老版本逻辑： 前端图片 - 后端url - 默认图片
    显示wallet icon逻辑： 后端url - 前端图片 - 默认图片
    
    Test Plan: 优先显示后端返回url，便于随时变更icon
    
    Reviewers: hwang, chendong, niegx, qding
    
    Reviewed By: niegx
    
    Subscribers: tql, cjiang
    
    Differential Revision: https://dev.bitpao.com/D2708

--- src/icons/WalletLogo/index.js
+++ src/icons/WalletLogo/index.js
@@ -3,13 +3,14 @@ import { Image } from 'react-native';
 import PropTypes from 'prop-types';
 import CoinLogos from '../CoinLogos';
 
+// 显示wallet icon逻辑： 后端url - 前端图片 - 默认图片
 function WalletLogo({ style, coin, uri }) {
+  if (uri && uri.length > 0) {
+    return <Image style={style} source={{ uri }} />;
+  }
   const coinIconSource = coin ? CoinLogos[coin] : '';
   if (coinIconSource) {
     return <Image style={style} source={coinIconSource} />;
-  }
-  if (uri && uri.length > 0) {
-    return <Image style={style} source={{ uri }} />;
   }
   return <Image style={style} source={require('./img/coin-default-logo.png')} />;
 }
