590eba73bc08fcb87e95b0071bdb7a7acc5a13fe
wufd <dong.wu@cobo.com>
2018-11-30 16:34:07 +0800

    update tx record Style
    Summary: update tx record UI
    
    Test Plan: fixed UI
    
    Reviewers: hwang, chendong, niegx, qding
    
    Reviewed By: niegx
    
    Differential Revision: https://dev.bitpao.com/D2858

--- src/components/TransactionRecordRow/index.js
+++ src/components/TransactionRecordRow/index.js
@@ -1,90 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components/native';
-import { Image, View } from 'react-native';
-import { CBText } from '../Core';
-import { SendIcon, ReceiveIcon, RewardIcon, ExchangeTx } from '../../icons';
 import TransactionStatus from '../TransactionStatus';
-
-const Container = styled.TouchableOpacity`
-  width: 100%;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffffff;
-  padding-left: 16;
-  padding-right: 16;
-  padding-top: 14;
-  padding-bottom: 14;
-`;
-
-const RecordInfo = styled.View`
-  flex-flow: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const MessageBox = styled.View`
-  flex: 1;
-  flex-flow: row;
-  align-items: center;
-  padding-right: 120;
-`;
-
-const Icon = ({ isSendOut, action, iconUrl, type }) => {
-  const styled = {
-    width: 40,
-    height: 40,
-    marginRight: 8,
-  };
-  if (iconUrl && iconUrl.length > 0) {
-    return <Image style={styled} source={{ uri: iconUrl }} />;
-  }
-  if (type && type.length > 0) {
-    switch (type) {
-      case 'exchange':
-        return <ExchangeTx style={styled} />;
-      case 'reward':
-        return <RewardIcon style={styled} />;
-      case 'send':
-        return <SendIcon type={'transaction'} style={styled} />;
-      case 'receive':
-        return <ReceiveIcon type={'transaction'} style={styled} />;
-    }
-  }
-  if (action === 'recv_pos_dividend' || action === 'recv_growth_dividend') {
-    return <RewardIcon style={styled} />;
-  }
-  if (action === 'send_exchange' || action === 'recv_exchange') {
-    return <ExchangeTx style={styled} />;
-  }
-  if (isSendOut) {
-    return <SendIcon type={'transaction'} style={styled} />;
-  } else {
-    return <ReceiveIcon type={'transaction'} style={styled} />;
-  }
-};
-
-const Amount = styled.View`
-  flex: 1;
-  flex-flow: row;
-  align-items: center;
-  justify-content: flex-end;
-`;
-
-const CoinAmount = styled(CBText)`
-  flex: 1;
-  text-align: right;
-  padding-left: 20;
-`;
-
-const TimeText = ({ time, style }) => {
-  return (
-    <CBText small color="grayLight" style={style} numberOfLines={1} ellipsizeMode={'middle'}>
-      {time}
-    </CBText>
-  );
-};
+import {
+  RecordIcon,
+  Container,
+  Content,
+  DescRow,
+  TitleRow,
+  TitleText,
+  AmountText,
+  Message,
+  TimeText,
+} from './style';
 
 const TransactionRecordRow = ({
   isSendOut,
@@ -104,35 +31,24 @@ const TransactionRecordRow = ({
   onPress,
 }) => (
   <Container onPress={onPress} style={style} disabled={!canPress} activeOpacity={0.6}>
-    {showIcon && <Icon isSendOut={isSendOut} type={type} action={action} iconUrl={iconUrl} />}
+    {showIcon && <RecordIcon isSendOut={isSendOut} type={type} action={action} iconUrl={iconUrl} />}
 
-    <View style={{ flex: 1 }}>
-      <RecordInfo style={{ marginBottom: 7 }}>
-        <CBText bold color="dark">
-          {title}
-        </CBText>
-        <Amount>
-          <CoinAmount superBold color={isSendOut ? 'dark' : 'primary'} numberOfLines={1}>{`${
-            isSendOut ? '-' : '+'
-          }${amount}`}</CoinAmount>
-          <CBText superBold color={isSendOut ? 'dark' : 'primary'}>
-            {' '}
-            {displayCode || coinCode}
-          </CBText>
-        </Amount>
-      </RecordInfo>
-      <RecordInfo>
-        <MessageBox>
-          <CBText small color="grayLight" style={{ marginRight: 2 }}>
-            {messageTitle}
-          </CBText>
-          <CBText small color="grayLight" numberOfLines={1}>
-            {message}
-          </CBText>
-        </MessageBox>
+    <Content>
+      <TitleRow>
+        <TitleText title={title} />
+        <AmountText
+          isSendOut={isSendOut}
+          amount={amount}
+          coinCode={coinCode}
+          displayCode={displayCode}
+        />
+      </TitleRow>
+
+      <DescRow>
+        <Message messageTitle={messageTitle} message={message} />
         {typeof extra === 'string' ? <TimeText time={extra} /> : <TransactionStatus {...extra} />}
-      </RecordInfo>
-    </View>
+      </DescRow>
+    </Content>
   </Container>
 );
 
---
+++ src/components/TransactionRecordRow/style.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Image } from 'react-native';
+import { CBText } from '../Core';
+import { SendIcon, ReceiveIcon, RewardIcon, ExchangeTx } from '../../icons';
+
+export const Container = styled.TouchableOpacity`
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  padding-left: 16;
+  padding-right: 16;
+  padding-top: 14;
+  padding-bottom: 14;
+`;
+
+export const Content = styled.View`
+  background-color: transparent;
+  align-items: stretch;
+  flex: 1;
+`;
+
+export const TitleRow = styled.View`
+  flex-flow: row;
+  align-items: center;
+`;
+
+export const DescRow = styled.View`
+  margin-top: 7;
+  flex-flow: row;
+  align-items: center;
+`;
+
+export const TitleText = ({ title }) => {
+  return (
+    <CBText bold color="dark" numberOfLines={1}>
+      {title}
+    </CBText>
+  );
+};
+
+const Amount = styled.View`
+  flex: 1;
+  flex-flow: row;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const CoinAmount = styled(CBText)`
+  flex: 1;
+  text-align: right;
+  padding-left: 16;
+`;
+
+export const AmountText = ({ isSendOut, amount, displayCode, coinCode }) => {
+  return (
+    <Amount>
+      <CoinAmount superBold color={isSendOut ? 'dark' : 'primary'} numberOfLines={1}>{`${
+        isSendOut ? '-' : '+'
+      }${amount}`}</CoinAmount>
+      <CBText superBold color={isSendOut ? 'dark' : 'primary'}>
+        {` ${displayCode || coinCode}`}
+      </CBText>
+    </Amount>
+  );
+};
+
+const MessageBox = styled.View`
+  flex: 1;
+  flex-flow: row;
+  align-items: center;
+  padding-right: 8;
+`;
+
+export const Message = ({ messageTitle, message }) => {
+  return (
+    <MessageBox>
+      {!!messageTitle && (
+        <CBText small color="grayLight" style={{ marginRight: 2 }} numberOfLines={1}>
+          {messageTitle}
+        </CBText>
+      )}
+
+      <CBText small color="grayLight" numberOfLines={1} style={{ flex: 1 }}>
+        {message}
+      </CBText>
+    </MessageBox>
+  );
+};
+
+export const TimeText = ({ time, style }) => {
+  return (
+    <CBText small color="grayLight" style={style} numberOfLines={1} ellipsizeMode={'middle'}>
+      {time}
+    </CBText>
+  );
+};
+
+export const RecordIcon = ({ isSendOut, action, iconUrl, type }) => {
+  const styled = {
+    width: 40,
+    height: 40,
+    marginRight: 8,
+  };
+  if (iconUrl && iconUrl.length > 0) {
+    return <Image style={styled} source={{ uri: iconUrl }} />;
+  }
+  if (type && type.length > 0) {
+    switch (type) {
+      case 'exchange':
+        return <ExchangeTx style={styled} />;
+      case 'reward':
+        return <RewardIcon style={styled} />;
+      case 'send':
+        return <SendIcon type={'transaction'} style={styled} />;
+      case 'receive':
+        return <ReceiveIcon type={'transaction'} style={styled} />;
+    }
+  }
+  if (action === 'recv_pos_dividend' || action === 'recv_growth_dividend') {
+    return <RewardIcon style={styled} />;
+  }
+  if (action === 'send_exchange' || action === 'recv_exchange') {
+    return <ExchangeTx style={styled} />;
+  }
+  if (isSendOut) {
+    return <SendIcon type={'transaction'} style={styled} />;
+  } else {
+    return <ReceiveIcon type={'transaction'} style={styled} />;
+  }
+};
